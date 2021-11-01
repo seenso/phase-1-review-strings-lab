d330035dd3a114cffe6c46a47b24c617e685deb4
seenso <>
2021-11-01 08:48:00 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Seen So";
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+let shortGreeting = "Welcome, " + currentUser[0] + "!";
